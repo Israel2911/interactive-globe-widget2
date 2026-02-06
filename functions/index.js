96153302548866245dbc41788e0bc19bf0a7e3c0
Israel2911 <148904270+Israel2911@users.noreply.github.com>
2026-02-07 04:10:57 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,18 @@
-// functions/index.js
-const functions = require('firebase-functions');
-const { createApp } = require('./server');
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
 
-const app = createApp();
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
 
-exports.globeApi = functions.https.onRequest(app);
+// Health check
+app.get("/", (req, res) => {
+  logger.info("Globe API health check", { structuredData: true });
+  res.status(200).send("Globe API is up");
+});
+
+// TODO: move your existing server.js routes into this app
+
+exports.globeApi = onRequest(app);
